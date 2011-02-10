156946b90df74ba529845c6938d01a61f8c8234f
Dr Nic Williams <drnicwilliams@gmail.com>
2011-02-10 13:54:29 -0800

    failing spec for issue #89 - 'ey recipes upload --apply'

--- spec/ey/recipes/upload_spec.rb
+++ spec/ey/recipes/upload_spec.rb
@@ -23,6 +23,29 @@ describe "ey recipes upload" do
   it_should_behave_like "it takes an environment name and an account name"
 end
 
+describe "ey recipes upload --apply" do
+  given "integration"
+
+  define_git_repo('+cookbooks') do |git_dir|
+    git_dir.join("cookbooks").mkdir
+    File.open(git_dir.join("cookbooks/file"), "w"){|f| f << "boo" }
+  end
+  use_git_repo('+cookbooks')
+
+  def command_to_run(opts)
+    cmd = "recipes upload --apply"
+    cmd << " --environment #{opts[:environment]}" if opts[:environment]
+    cmd << " --account #{opts[:account]}" if opts[:account]
+    cmd
+  end
+
+  def verify_ran(scenario)
+    @out.should =~ /Recipes uploaded and applied successfully for #{scenario[:environment]}/
+  end
+
+  it_should_behave_like "it takes an environment name and an account name"
+end
+
 describe "ey recipes upload with an ambiguous git repo" do
   given "integration"
   def command_to_run(_) "recipes upload" end
